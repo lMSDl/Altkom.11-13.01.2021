c417fc86f13bcc194dfcc0e2a6b2d861d846ae76
Paweł Łęczycki <pawel@msd.info.pl>
2021-01-11 14:24:49 +0100

    Lambda

--- CSharp/ConsoleApp/Program.cs
+++ CSharp/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 using ConsoleApp.Delegates;
+using ConsoleApp.LambdaExpression;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,33 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            var example = new BuildInDelegatesExample();
+            var example = new LambdaExamples();
 
             example.Test();
             Console.ReadKey();
+        }
+
+        static void Abc()
+        {
+            Nullable<int> a = null;
+            int? b = 5;
+            int c;
+
+            if(a - b == 0)
+            {
+                c = (a + b) ?? 0;
+            }
+            else
+            {
+                var result = a - b;
+                //if (result != null)
+                if(result.HasValue)
+                    c = result.Value;
+                else
+                    c = 0;
+            }
+
+            c = (a - b == 0 ? a + b : a - b) ?? 0;
         }
     }
 }
